185a3eadb4568abfaa2e298da4e50cb664518c93
billsioros <billsioros97@gmail.com>
2018-08-29 15:16:35 +0300

    Updated Average

--- test/average.sh
+++ test/average.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -lt 4 ]
 then
-    echo "<ERR>: "$0" <executable> <delimeter> <iterations> <output> <recreate>!"
+    echo "<MSG>: "$0" <executable> <delimeter> <iterations> <output> <recreate>"
     exit 1
 fi
 
@@ -10,8 +10,14 @@ executable="$1"; delimeter="$2"; iterations="$3"; output="$4"
 
 if [ ! -x "$executable" ]
 then
-    echo "<ERR>: "$executable" is not an executable file!"
+    echo "<ERR>: "$executable" is not an executable file"
     exit 2
+fi
+
+if ! [[ "$iterations" =~ ^[0-9]+$ ]] || [ "$iterations" -le 0 ]
+then
+    echo "<ERR>: The number of iterations must be a positive integer"
+    exit 4
 fi
 
 if [ "$#" -ge 5 ]
@@ -26,14 +32,8 @@ fi
 
 if [ ! -w "$output" ]
 then
-    echo "<ERR>: "$output" is not a writeable file!"
+    echo "<ERR>: "$output" is not a writeable file"
     exit 3
-fi
-
-if ! [[ "$iterations" =~ ^[0-9]+$ ]] || [ "$iterations" -le 0 ]
-then
-    echo "<ERR>: The number of iterations must be a positive integer!"
-    exit 4
 fi
 
 total=0
@@ -42,7 +42,7 @@ ratios=$(grep "$delimeter" "$output" | cut -f2 -d ":")
 
 if [ -z "$ratios" ]
 then
-    echo "<ERR>: "$delimeter" does not appear anywhere inside "$output"!"
+    echo "<ERR>: "$delimeter" does not appear anywhere inside "$output""
     exit 4
 fi
 
